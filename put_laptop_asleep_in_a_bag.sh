cd4ace37ca34359f3eeda517acc9e11735f0f52c
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2022-01-25 15:03:56 +0100

    Add files via upload

---
+++ put_laptop_asleep_in_a_bag.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Nice idea by Jay LaCroix to put laptop asleep in a bag.
+# Set it like cron job for 2 minutes check. The second check is to prevent suspension while the laptop is closed but in the dock.
+grep -q closed /proc/acpi/button/lid/LID/state
+
+if [ $? = 0 ]; then
+	## The laptop is closed, let's find out if it's plugged in
+	grep -q 0 cat /sys/class/power_supply/AC/online
+	if [ $? = 0 ]
+	then
+		# The laptop is not plugged in. It should be asleep.
+		pm-suspend
+	fi
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
